75f693d58ef4e526841a82e1cfa3cdd7629d9fb9
howsetya <howsetya@googlemail.com>
2026-04-29 19:31:50 +0200

    comment out filtering functionality due to jittering on display. Other streamlining.

--- frontend/src/types/artist.ts
+++ frontend/src/types/artist.ts
@@ -5,13 +5,14 @@ export interface Genre { //separate interface if genres are their own entities i
 }
 
 export interface Artist { //should match the shape returned by GET /artists/:id from express (used in ArtistPage)
-  id:          string //our id
-  raId:        string //ra's id
-  name:        string
-  genres:      Genre[] //array to display both name and use slug for filtering without extra lookups
-  bio?:        string
-  claimed:     boolean //if profile is claimed to be used later to show an "Edit profile" to real artist
-  eventCount?: number
+  id:             string //sg_id
+  raId?:           string //ra's id maybe unnecessary
+  name:           string
+  genres:         Genre[] //array to display both name and use slug for filtering without extra lookups
+  bio?:           string
+  claimed:        boolean //if profile is claimed to be used later to show an "Edit profile" to real artist
+  eventCount?:    number
+  artistLinks?:   string[] //artists
 }
 
 export interface SimilarArtist { //used by GET /artists/:id/similar
