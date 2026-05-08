215ebd79de3f358bfc6fdea044cca9a21dc926c1
tghnx1 <kokorevm2012@gmail.com>
2026-05-08 19:40:42 +0200

    Clean project structure and move Prisma under backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,37 +1,3 @@
-/* import { Routes, Route, Navigate, NavLink } from 'react-router-dom'
-import { GraphPage }  from './pages/GraphPage'
-import { DashboardPage }    from './pages/DashboardPage'
-import { ArtistPage } from './pages/ArtistPage'
-
-export default function App() {
-  return (
-    <div style={{ display: 'flex', flexDirection: 'column', height: '100vh' }}>
-      <nav
-        style={{
-          padding: '12px 20px',
-          borderBottom: '1px solid #eee',
-          display: 'flex',
-          gap: '16px',
-        }}
-      >
-        <NavLink to="/graph">Graph</NavLink>
-        <NavLink to="/dashboard">Dashboard</NavLink>
-      </nav>
-
-      <main style={{ flex: 1, overflow: 'hidden' }}>
-        <Routes>
-          <Route path="/" element={<Navigate to="/graph" />} />
-          <Route path="/graph" element={<GraphPage />} />
-          <Route path="/dashboard" element={<DashboardPage />} />
-          <Route path="/artist/:id" element={<ArtistPage />} />
-        </Routes>
-      </main>
-    </div>
-  )
-}
- */
-
-//maybe better colours
 import type { CSSProperties, ReactNode } from 'react'
 import { Routes, Route, Navigate, NavLink, Link } from 'react-router-dom'
 import { GraphPage } from './pages/GraphPage'
